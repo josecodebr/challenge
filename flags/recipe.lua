11e543b7c0169f7374547a5675405bccb1d5877f
JoseCodeBR <joseanastaciodiascastilho@gmail.com>
2026-09-04 21:57:25 -0300

    Update crafting recipes for flags and circles
    Refactor recipes for flag points and challenge circles, adding color variations and automatic recipe generation.

--- flags/recipe.lua
+++ flags/recipe.lua
@@ -1,55 +1,48 @@
---flag_point recipe
---white
+-- flags/recipe.lua
+-- 1. Receitas Principais (Item Base Branco)
 minetest.register_craft({
-	output = "challenge:tool_flag_point_3d_white",
-	recipe = {
-                  {"group:leaves",	"group:leaves",		"group:leaves"},
-                  {"group:leaves",	"default:mese_crystal",	"group:leaves"},
-                  {"group:leaves",	"group:leaves",		"group:leaves"},
-			}})
+    output = "challenge:tool_flag_point_3d_white",
+    recipe = {
+        {"group:leaves", "group:leaves",          "group:leaves"},
+        {"group:leaves", "default:mese_crystal", "group:leaves"},
+        {"group:leaves", "group:leaves",          "group:leaves"},
+    }
+})
 
---black
-minetest.register_craft({ output = "challenge:tool_flag_point_3d_black", recipe = {
-						{"challenge:tool_flag_point_3d_white", "default:coal_lump"},} })
---red
-minetest.register_craft({ output = "challenge:tool_flag_point_3d_red", recipe = {
-						{"challenge:tool_flag_point_3d_white", "flowers:rose"},} })
-
---blue
-minetest.register_craft({ output = "challenge:tool_flag_point_3d_blue", recipe = {
-						{"challenge:tool_flag_point_3d_white", "flowers:geranium"},} })
---green
-minetest.register_craft({ output = "challenge:tool_flag_point_3d_green", recipe = {
-						{"challenge:tool_flag_point_3d_white", "default:cactus"},} })
---yellow
-minetest.register_craft({ output = "challenge:tool_flag_point_3d_yellow", recipe = {
-						{"challenge:tool_flag_point_3d_white", "flowers:dandelion_yellow"},} })
-------------------------------------------------------------------------------------------------------------------------------
-
-
---checkpoints recipe
---white
 minetest.register_craft({
-	output = "challenge:tool_challenge_circle_open_white",
-	recipe = {
-                  {"group:leaves",	"group:leaves",		"group:leaves"},
-                  {"group:leaves",	"default:diamond",	"group:leaves"},
-                  {"group:leaves",	"group:leaves",		"group:leaves"},
-			}})
+    output = "challenge:tool_challenge_circle_open_white",
+    recipe = {
+        {"group:leaves", "group:leaves",      "group:leaves"},
+        {"group:leaves", "default:diamond",  "group:leaves"},
+        {"group:leaves", "group:leaves",      "group:leaves"},
+    }
+})
 
---black
-minetest.register_craft({ output = "challenge:tool_challenge_circle_open_black", recipe = {
-						{"challenge:tool_challenge_circle_open_white", "default:coal_lump"},} })
---red
-minetest.register_craft({ output = "challenge:tool_challenge_circle_open_red", recipe = {
-						{"challenge:tool_challenge_circle_open_white", "flowers:rose"},} })
+-- 2. Tabela de Ingredientes de Tingimento por Cor
+local color_ingredients = {
+    black  = "default:coal_lump",
+    red    = "flowers:rose",
+    blue   = "flowers:geranium",
+    green  = "default:cactus",
+    yellow = "flowers:dandelion_yellow",
+}
 
---blue
-minetest.register_craft({ output = "challenge:tool_challenge_circle_open_blue", recipe = {
-						{"challenge:tool_challenge_circle_open_white", "flowers:geranium"},} })
---green
-minetest.register_craft({ output = "challenge:tool_challenge_circle_open_green", recipe = {
-						{"challenge:tool_challenge_circle_open_white", "default:cactus"},} })
---yellow
-minetest.register_craft({ output = "challenge:tool_challenge_circle_open_yellow", recipe = {
-						{"challenge:tool_challenge_circle_open_white", "flowers:dandelion_yellow"},} })
+-- 3. Loop Automático para Gerar Receitas de Tingimento
+local base_items = {
+    "flag_point_3d",
+    "challenge_circle_open"
+}
+
+for _, item_base in ipairs(base_items) do
+    local white_item = "challenge:tool_" .. item_base .. "_white"
+
+    for color, ingredient in pairs(color_ingredients) do
+        local colored_item = "challenge:tool_" .. item_base .. "_" .. color
+
+        minetest.register_craft({
+            type = "shapeless",
+            output = colored_item,
+            recipe = {white_item, ingredient},
+        })
+    end
+end
