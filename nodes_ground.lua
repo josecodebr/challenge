8da62ef87914790be480be2860532f122ea81084
JoseCodeBR <joseanastaciodiascastilho@gmail.com>
2026-09-04 21:52:14 -0300

    Refactor ground node registration with color data

--- nodes_ground.lua
+++ nodes_ground.lua
@@ -1,46 +1,57 @@
-local cores={
-{"white","#FFFFFF70"},
-{"red","#AA000070"},
-{"blue","#0000AA70"},
-{"green","#00ff0070"},
+-- nodes_ground.lua
+
+local cores = {
+    {"white", "#FFFFFF70"},
+    {"red",   "#AA000070"},
+    {"blue",  "#0000AA70"},
+    {"green", "#00ff0070"},
 }
 
-local function is_ground(pos)
-  local nn = minetest.get_node(pos).name
-return minetest.get_item_group(nn, "stone") ~= 0
-end 
+for _, color_data in ipairs(cores) do
+    local color_name = color_data[1]
+    local color_hex  = color_data[2]
 
-for i = 1, #cores, 1 do
+    minetest.register_node("challenge:ground_" .. color_name, {
+        description = "Ground " .. color_name:sub(1,1):upper() .. color_name:sub(2),
+        tiles = {"ground.png^[colorize:" .. color_hex},
+        light_source = 5,
+        walkable = true,
+        paramtype = "light",
+        is_ground_content = true,
+        groups = {cracky = 2, oddly_breakable_by_hand = 2, soil = 1},
 
-minetest.register_node("challenge:ground_"..cores[i][1], {
-	description = "ground_"..cores[i][1],
-    tiles = {"ground.png^[colorize:"..cores[i][2]},
-	light_source = 5,
-	walkable=true,
-	paramtype = "light",is_ground_content = true,
-	groups = {cracky = 2, oddly_breakable_by_hand = 2, soil = 1},
+        after_place_node = function(pos, placer)
+            local timer = minetest.get_node_timer(pos)
+            timer:start(0.5) -- 0.5s já é extremamente responsivo e consome bem menos CPU que 0.2s
+        end,
 
-after_place_node = function (pos, placer)
-			local timer = minetest.get_node_timer(pos)
-			timer:start(0.2)
-end,
+        on_timer = function(pos, node)
+            local check_pos = vector.add(pos, vector.new(0, 1, 0))
+            local objects = minetest.get_objects_inside_radius(check_pos, 0.8)
 
-on_timer = function(pos,node)
-		local timer = minetest.get_node_timer(pos)
-		timer:start(0.2) 
-		local meta = minetest.get_meta(pos)
-		local throw_starting_pos={}
-		throw_starting_pos = vector.add({x=0, y=1, z=0},pos)
-		local all_objects = minetest.get_objects_inside_radius(throw_starting_pos, 0.5)
-		local players = {}
-			local _,obj for _,obj in ipairs(all_objects) do
-				if obj:is_player() then 
-					local player_name = obj:get_player_name()
-					meta:set_string("infotext", ("Chekpoint: "..player_name))
-					table.insert(players, obj) 
-					if #players == 1 then
-					minetest.sound_play("catch2", {pos=pos, gain = 1.0, max_hear_distance = 5})
-		end end end
-	end,
-})
+            for _, obj in ipairs(objects) do
+                if obj:is_player() then
+                    local player_name = obj:get_player_name()
+                    local meta = minetest.get_meta(pos)
+
+                    -- Atualiza o infotext apenas se for um jogador diferente (evita reescrever a meta sem necessidade)
+                    local current_info = meta:get_string("infotext")
+                    local new_info = "Checkpoint: " .. player_name
+
+                    if current_info ~= new_info then
+                        meta:set_string("infotext", new_info)
+                        minetest.sound_play("catch2", {
+                            pos = pos,
+                            gain = 1.0,
+                            max_hear_distance = 5
+                        })
+                    end
+
+                    break -- Já encontrou um jogador, interrompe o loop
+                end
+            end
+
+            return true -- Mantém o timer ativo no mesmo intervalo configurado
+        end,
+    })
 end
